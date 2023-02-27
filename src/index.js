7a31f1a16e5e5ce871cc6b8dd6b7ed6470e118fb
MUmerIdrees <122514170+MUmerIdrees@users.noreply.github.com>
2023-02-27 11:29:34 +0500

    update index.js

--- src/index.js
+++ src/index.js
@@ -3,17 +3,17 @@ import { render } from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
-<<<<<<< HEAD
+ 
 import { Elements } from '@stripe/react-stripe-js';
 
 import App from './App';
 import { store, persistor } from './store/store';
 import { stripePromise } from './utils/stripe/stripe.utils';
-=======
+ 
 
 import App from './App';
 import { store, persistor } from './store/store';
->>>>>>> 6d935f97ce22422aa0aa5a51ebc05ea05351c844
+ 
 
 import './index.scss';
 
@@ -24,13 +24,10 @@ render(
     <Provider store={store}> 
       <PersistGate loading={null} persistor={persistor}> 
         <BrowserRouter>
-<<<<<<< HEAD
+ 
           <Elements stripe={stripePromise}>
             <App />
           </Elements>
-=======
-          <App />
->>>>>>> 6d935f97ce22422aa0aa5a51ebc05ea05351c844
         </BrowserRouter>
       </PersistGate>
     </Provider>
