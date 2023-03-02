3c0973cdb2b073832e0e821c67e985624a69ff17
umer <umer19960@gmail.com>
2023-03-02 17:27:24 +0500

    convert app over to typescript

---
+++ src/components/directory/directory.component.tsx
@@ -0,0 +1,57 @@
+import { Key } from 'react';
+
+import DirectoryItem from '../directory-item/directory-item.component';
+
+import { DirectoryContainer } from './directory.styles';
+
+export type DirectoryCategory = {
+  id: Key;
+  title: string;
+  imageUrl: string;
+  route: string;
+};
+
+const categories: DirectoryCategory[] = [
+    {
+      id: 1,
+      title: 'hats',
+      imageUrl: 'https://i.ibb.co/tHxZ9hN/hat.jpg',
+      route: 'shop/hats'
+    },
+    {
+      id: 2,
+      title: 'jackets',
+      imageUrl: 'https://i.ibb.co/BGWjwSr/jacket.jpg',
+      route: 'shop/jackets'
+    },
+    {
+      id: 3,
+      title: 'sneakers',
+      imageUrl: 'https://i.ibb.co/SJ5r75r/sneaker.jpg',
+      route: 'shop/sneakers'
+    },
+    {
+      id: 4,
+      title: 'womens',
+      imageUrl: 'https://i.ibb.co/N3wCnHV/women.jpg',
+      route: 'shop/womens'
+    },
+    {
+      id: 5,
+      title: 'mens',
+      imageUrl: 'https://i.ibb.co/gt8V3dF/mens.jpg',
+      route: 'shop/mens'
+    },
+];
+
+const Directory = () => {
+  return (
+    <DirectoryContainer>
+      {categories.map((category) => (
+        <DirectoryItem key={category.id} category={category} />
+      ))}
+    </DirectoryContainer>
+  );
+};
+
+export default Directory;
